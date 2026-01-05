d6f244f9432a212a8b71efea62b0e37a8066b0a0
karimaulya <karimaulya13@hmail.com>
2026-01-05 11:35:32 +0700

    private api

--- cek_model.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# Ganti ini sama API KEY kamu yang tadi
-genai.configure(api_key="AIzaSyBFv6JUriAwceCGEo-Yyc2aisjWnoVxmuQ") 
-
-print("Mencari model yang tersedia...")
-for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)
